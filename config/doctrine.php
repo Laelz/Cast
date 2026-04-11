ae6c71b53de0011d8bceedc174da659968860ac2
Lael Albuquerque <lael_albuquerque@hotmail.com>
2026-04-11 17:52:22 -0300

    Doctrine config

--- config/doctrine.php
+++ config/doctrine.php
@@ -1,7 +1,5 @@
 <?php
 
-
-use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
@@ -10,14 +8,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $paths = [__DIR__ . '/../app/Entities'];
 $isDevMode = true;
 
-// por enquanto vamos usar annotations/metadata clássica
-$config = Setup::createAnnotationMetadataConfiguration(
-    $paths,
-    $isDevMode,
-    null,
-    null,
-    false
-);
+$config = Setup::createAttributeMetadataConfiguration($paths, $isDevMode);
 
 $connectionParams = [
     'driver'   => $_ENV['DB_DRIVER'],
@@ -27,7 +18,5 @@ $connectionParams = [
     'user'     => $_ENV['DB_USER'],
     'password' => $_ENV['DB_PASS'],
 ];
-
-$connection = DriverManager::getConnection($connectionParams, $config);
 
 return EntityManager::create($connectionParams, $config);
